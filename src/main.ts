4c66e006f1e85e4cff1a5051c36a7cf68e145a22
wangxinlei <wxl@wangxinlei.cn>
2021-12-25 20:04:24 +0800

    build: 添加element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import './style/index.scss'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installPlugins from '@/plugins'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installPlugins(app)
+app.use(store).use(router).mount('#app')
---
+++ src/plugins/index.ts
@@ -0,0 +1,6 @@
+import { App } from 'vue'
+import installElementPlus from './element'
+
+export default (app: App): void => {
+  installElementPlus(app)
+}
